2bcacbbe7649c52898d52e175d390c6d46f459b8
Adrian Ortiz Velez <aortizsax@gmail.com>
2023-03-09 17:18:09 -0800

    Housekeeping

--- scripts/2023-01-04_calccutoff.py
+++ scripts/2023-01-04_calccutoff.py
@@ -309,7 +309,7 @@ if __name__ == "__main__":
         plt.axvline(x=cutoffabsent, color="red")
         plt.xlim([0, dimen[1]])
 #        plt.savefig(path + "/"+ID+"processingcutoff.png")
-        plt.savefig(path + "/"+ID+"processingcutoffB.svg", format='svg',dpi=1200)
+        plt.savefig(path + "/"+ID+"processingcutoffB.png", format='png',dpi=1200)
         plt.show()
 
         
@@ -409,5 +409,5 @@ if __name__ == "__main__":
         plt.axvline(x=cutoff, color="red")
         plt.xlim([0, 50])
 #        plt.savefig(path + "/"+ID+"processingcutoff.png")
-        plt.savefig(path + "/"+ID+"processingcutoffB.svg", format='svg',dpi=1200)
+        plt.savefig(path + "/"+ID+"processingcutoffB.png", format='png',dpi=1200)
         plt.show()
